93fafd660a4c5921508bbf27f14ed7bdcbc51a17
Priyanshu-cryenx <priyanshudash001@gmail.com>
2024-07-17 23:44:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/quiz/QuestionActivity.java
@@ -0,0 +1,133 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionActivity extends AppCompatActivity {
+    RadioGroup radioGroup;
+    TextView lblQuestion;
+    RadioButton optionA;
+    RadioButton optionB;
+    RadioButton optionC;
+    RadioButton optionD;
+    Button confirm;
+    String rightAnswer;
+    String Answer;
+    List<Question> questions;
+    int score;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+        confirm = findViewById(R.id.confirm);
+        lblQuestion = findViewById(R.id.lblPergunta);
+        optionA = findViewById(R.id.opcaoA);
+        optionB = findViewById(R.id.opcaoB);
+        optionC = findViewById(R.id.opcaoC);
+        optionD = findViewById(R.id.opcaoD);
+        score = 0;
+        radioGroup = findViewById(R.id.radioGroup);
+
+        questions = new ArrayList<Question>(){
+            {
+                add(new Question("Under which of the following Android is licensed?", "D", "Sourceforge", "OSS","None of the above", "Apache/MIT"));
+                add(new Question("For which of the following Android is mainly developed?", "A", "Mobile devices", "Laptops","Desktops", "Servers"));
+                add(new Question("Which of the following is the first mobile phone released that ran the Android OS?", "D", "HTC Hero", "Google gPhone","None of the above", "T - Mobile G1"));
+                add(new Question("Which of the following virtual machine is used by the Android operating system?", "B", "JVM", "Dalvik virtual machine", "Simple virtual machine", "None of the above"));
+                add(new Question(" Android is based on which of the following language?", "A", "Java", "C++", "C", "None of the above"));
+                add(new Question("APK stands for -", "D", "None of the above", "Android Phone Kit", "Android Page Kit", "Android Package Kit"));
+                add(new Question("Which of the following converts Java byte code into Dalvik byte code?", "C", "Dalvik converter", "Mobile interpretive compiler (MIC)", "Dex compiler", "None of the above"));
+                add(new Question("How can we stop the services in android?", "B", "By using the finish() method", "By using the stopSelf() and stopService() method", "By using system.exit() method", "None of the above"));
+                add(new Question("What is an activity in android?", "B", "android class", "A single screen in an application with supporting java code", "android package", "None of the above"));
+                add(new Question("How can we kill an activity in android?", "D", "Using finish() method", "Using finishActivity(int requestCode)", "Neither (a) nor (b)", "Both (a) and (b)"));
+            }
+        };
+
+        loadQuestion();
+    }
+
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        loadQuestion();
+    }
+
+
+    private void loadQuestion(){
+        if(questions.size() > 0) {
+            Question q = questions.remove(0);
+            lblQuestion.setText(q.getQuestion());
+            List<String> answers = q.getAnswers();
+
+            optionA.setText(answers.get(0));
+            optionB.setText(answers.get(1));
+            optionC.setText(answers.get(2));
+            optionD.setText(answers.get(3));
+
+            rightAnswer = q.getRightAnswer();
+        } else {
+            Intent intent = new Intent(this, ShowScoreActivity.class);
+            intent.putExtra("score", score);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public void loadAnswer(View view) {
+        int op = radioGroup.getCheckedRadioButtonId();
+
+        switch (op){
+            case R.id.opcaoA:
+                Answer="A";
+                break;
+
+            case R.id.opcaoB:
+                Answer="B";
+                break;
+
+            case R.id.opcaoC:
+                Answer="C";
+                break;
+
+            case R.id.opcaoD:
+                Answer="D";
+                break;
+
+            default:
+                return;
+
+        }
+
+        radioGroup.clearCheck();
+
+        this.startActivity(isRightOrWrong(Answer));
+
+    }
+
+    private Intent isRightOrWrong(String Answer){
+        Intent screen;
+        if(Answer.equals(rightAnswer)) {
+            this.score += 1;
+            screen = new Intent(this, RightActivity.class);
+
+        }else {
+            screen = new Intent(this, WrongActivity.class);
+        }
+
+        return screen;
+    }
+}
